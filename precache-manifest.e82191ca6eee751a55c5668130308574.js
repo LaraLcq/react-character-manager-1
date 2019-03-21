2677e192d667ec819369f3564f4039e063afae3e
bovealexandre <alexandre.l.bove@gmail.com>
2019-03-21 11:29:24 +0100

    Updates

---
+++ precache-manifest.e82191ca6eee751a55c5668130308574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "c5dbbf60bca6b66ba41f",
+    "url": "/react-character-manager/static/js/main.1fa07376.chunk.js"
+  },
+  {
+    "revision": "7adb62a4c7f7b2636fce",
+    "url": "/react-character-manager/static/js/2.2421f562.chunk.js"
+  },
+  {
+    "revision": "c5dbbf60bca6b66ba41f",
+    "url": "/react-character-manager/static/css/main.faf3b57e.chunk.css"
+  },
+  {
+    "revision": "7adb62a4c7f7b2636fce",
+    "url": "/react-character-manager/static/css/2.5d99afa1.chunk.css"
+  },
+  {
+    "revision": "142b6e08d6c5ab4102e76d74567c7839",
+    "url": "/react-character-manager/index.html"
+  }
+];
